e8ea761d649e0827988a02056f11f20039ebfe20
RX Database <cbriggs@mcoa.com>
2018-03-19 08:52:27 -0400

    serveyou seeds 03192018

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', '301optim20180316.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', '20180316SYLoad.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  t = Wellmont.new
+  t = Claim.new
   t.lName = row['lName']
   t.fName = row['fName']
   t.bDate = row['bDate']
@@ -25,4 +25,4 @@ csv.each do |row|
   puts "#{t.lName}, #{t.fName}, #{t.bDate}, #{t.groupId}, #{t.sDate}, #{t.drug}, #{t.planPaid}, #{t.coPay}, #{t.totalPaid} saved"
 end
 
-puts "There are now #{Wellmont.count} rows in the Wellmont table"
+puts "There are now #{Claim.count} rows in the Wellmont table"
